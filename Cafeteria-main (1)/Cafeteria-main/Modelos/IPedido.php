42e277332ba12ecd29df398b94c0df95907d6288
Heloisa T. L. <134729931+heloisatl@users.noreply.github.com>
2024-11-21 14:20:22 -0300

    Delete Cafeteria-main (1)/Cafeteria-main directory

--- Cafeteria-main (1)/Cafeteria-main/Modelos/IPedido.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface IPedido {
-    public function adicionarProduto(Produto $produto): void;
-    public function listarProdutosPedido(): void;
-    public function calcularTotal(): float;
-    public function adicionarPedido(Pedido $pedido): void;
-}
